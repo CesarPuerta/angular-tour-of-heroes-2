74e20c4278f0c97e3e8f39bb857817fbd9f251a8
Cesar Puerta <cesar.puerta@yuxiglobal.com>
2017-07-21 21:06:00 -0500

    Fixing redirectTo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '', component: DashboardComponent, pathMatch: 'full' },
   { path: 'dashboard',  component: DashboardComponent },
   { path: 'detail/:id', component: HeroDetailsComponent },
   { path: 'heroes',     component: HeroComponent }
