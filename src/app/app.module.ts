e5d194b93db972170264aadc690c7e667dadfdec
Cesar Puerta <cesar.puerta@yuxiglobal.com>
2017-07-21 20:32:51 -0500

    HTTP Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
 import { HeroDetailsComponent } from './hero/hero-details/hero-details.component';
 import { DashboardComponent } from './hero/dashboard/dashboard.component';
 import { HeroComponent } from './hero/hero/hero.component';
 import { HeroModule } from './hero/hero.module';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
-
-import { AppComponent } from './app.component';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({
   imports: [
     BrowserModule,
     HeroModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
   ],
   declarations: [
     AppComponent
--- src/app/hero/hero.module.ts
+++ src/app/hero/hero.module.ts
@@ -5,19 +5,29 @@ import { CommonModule } from '@angular/common';
 import { HeroDetailsComponent } from './hero-details/hero-details.component';
 import { HeroComponent } from './hero/hero.component';
 import { FormsModule } from '@angular/forms';
-import { DashboardComponent } from './dashboard/dashboard.component'
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component'
+import { HeroSearchService } from './hero-search.service';
+import { RouterModule } from '@angular/router';
+
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    RouterModule
   ],
   declarations: [
     HeroDetailsComponent,
     HeroComponent,
-    DashboardComponent],
+    DashboardComponent,
+    HeroSearchComponent
+  ],
   exports: [
     HeroComponent
   ],
-  providers: [HeroesService]
+  providers: [
+    HeroesService,
+    HeroSearchService
+  ]
 })
 export class HeroModule { }
